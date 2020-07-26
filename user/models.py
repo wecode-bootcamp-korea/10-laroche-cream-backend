d2d4c829e854449e2b2214a08eb14af5446e3a3b
sol ahn <solahn@MacBook-Pro-5.local>
2020-07-26 14:24:10 +0900

    Fix: models.py 수정

--- user/models.py
+++ user/models.py
@@ -1,35 +1,44 @@
 from django.db import models
 
+class User(models.Model):
+    name        = models.CharField(max_length = 50, default = "")
+    account     = models.CharField(max_length = 100, default = "")
+    password    = models.CharField(max_length = 100, default = "")
+    birthday    = models.CharField(max_length = 100, null = True)
+    gender_type = models.ForeignKey('Gender', on_delete = models.CASCADE, null=True)
+    email       = models.CharField(max_length = 200, default = "")
+    phoneNumber = models.CharField(max_length = 100, default = "") 
+    skinType    = models.ForeignKey('SkinType', on_delete = models.CASCADE, null=True)
+    skinTrouble = models.ManyToManyField('SkinTrouble', through = 'UserSkinTrouble')
+    skinSensitivity = models.IntegerField(default = 0)
+    created_at  = models.DateTimeField(auto_now_add = True, null=True)
+    updated_at  = models.DateTimeField(auto_now = True, null=True)
+    
+    class Meta:
+        db_table = "users"
+
 class Gender(models.Model):
     name = models.CharField(max_length=10)
 
     class Meta:
         db_table = "genders"
 
-class User(models.Model):
-    name = models.CharField(max_length=50, default=" ")
-    account = models.CharField(max_length=100, default=" ")
-    password = models.CharField(max_length=100, default=" ")
-    birthday = models.DateField(default=0)
-    gender_type = models.ForeignKey('Gender', on_delete=models.SET_NULL, null=True)
-    email = models.CharField(max_length=200, default=" ")
-    phoneNumber = models.CharField(max_length=100, default=" ")
-    skinInfo = models.ForeignKey('SkinInfo', on_delete=models.SET_NULL, null=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
-    
-    class Meta:
-        db_table = "users"
-
-
-class SkinInfo(models.Model):
-    skinType = models.CharField(max_length=50, null=True)
-    skinTrouble = models.CharField(max_length=100, null=True)
-    skinSensitivity = models.IntegerField(default=0)
+class SkinType(models.Model):
+    name = models.CharField(max_length = 50, null=True)
 
     class Meta:
-        db_table = "skin_info"
+        db_table = "skin_types"
 
+class UserSkinTrouble(models.Model):
+    user = models.ForeignKey('User', on_delete = models.CASCADE)
+    skinTrouble = models.ForeignKey('SkinTrouble', on_delete = models.CASCADE)
 
+    class Meta:
+        db_table = "user_skin_troubles"
 
+class SkinTrouble(models.Model):
+    name = models.CharField(max_length = 50, default = " ")
+
+    class Meta:
+        db_table = "skin_troubles"
 
--- user/views.py
+++ user/views.py
@@ -3,12 +3,20 @@ import json
 import bcrypt
 import jwt
 
-from django.shortcuts import render
 from django.views     import View  
-from django.http      import HttpResponse, JsonResponse
+from django.http      import (
+    HttpResponse, 
+    JsonResponse
+)    
 
 from my_settings      import SECRET_KEY
-from user.models      import User, Gender, SkinInfo
+from user.models      import (
+    User, 
+    Gender, 
+    SkinType,
+    UserSkinTrouble,
+    SkinTrouble
+)
 
 
 class SignUpView(View):
@@ -18,21 +26,24 @@ class SignUpView(View):
             if User.objects.filter(account = user_data['account']).exists():
                 return JsonResponse({'message':'DUPLICATED ID'}, status=400)
 
-            User.objects.create(
+             len(user_data['account']) 
+
+            User(
                 name        = user_data['name'],
                 account     = user_data['account'],
                 password    = bcrypt.hashpw(user_data['password'].encode('utf-8'), bcrypt.gensalt()).decode(),
                 email       = user_data['email'],
-                birthday    = datetime.datetime.strptime(user_data['birthday'], "%Y%m%d").date(),
+                birthday    = user_data['birthday'],
                 gender_type = Gender.objects.get(name = user_data['gender_type']),
                 phoneNumber = user_data['phoneNumber'],
+                skinType = SkinType.objects.get(name = user_data['skinType']),
+                skinSensitivity = user_data['skinSensitivity']
             ).save()
 
+            UserSkinTrouble(
+                user = User.objects.get(account = user_data['account']),
+                skinTrouble = SkinTrouble.objects.get(name = user_data['skinTrouble'])
 
-            SkinInfo(
-                skinType        = user_data['skinType'],
-                skinTrouble     = user_data['skinTrouble'],
-                skinSensitivity = user_data['skinSensitivity']
             ).save()
 
 
@@ -48,21 +59,15 @@ class SignInView(View):
         try:
             if User.objects.filter(account = user_data['account']).exists():
                 user = User.objects.get(account = user_data['account'])
-
+ 
                 if bcrypt.checkpw(user_data['password'].encode('utf-8'), user.password.encode('utf-8')):
-                    token = jwt.encode({'user_id':user.id}, 'secret', algorithm='HS256')
-                    token = token.decode('utf-8')
+                    token = jwt.encode({'user_id':user.id}, 'secret', algorithm='HS256').decode('utf-8')
                     return JsonResponse({'access_token':token}, status=200)
-                else:
-                    return JsonResponse({'message':'UNAUTHORIZED'}, status=401)
+                
+                return JsonResponse({'message':'UNAUTHORIZED'}, status=401)
 
-            return JsonResponse({'message':'INVALID USER'}, status=400)
+            return JsonResponse({'message':'INVALID_USER'}, status=400)
 
         except KeyError:
-            return JsonResponse({'message':'KeyError'}, status=400)
+            return JsonResponse({'message':'KEYERROR'}, status=400)
             
-
-
-
-
-
