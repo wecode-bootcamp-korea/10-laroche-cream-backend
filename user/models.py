b052e38df8acb3bb417266a261b66e9dc63cb597
sol ahn <solahn@MacBook-Pro-5.local>
2020-07-30 17:24:08 +0900

    Add: 찜하기, 장바구니 엔드포인트 구현

--- user/models.py
+++ user/models.py
@@ -1,18 +1,20 @@
 from django.db import models
+from product.models import *
 
 class User(models.Model):
-    name        = models.CharField(max_length = 50, default = "")
-    account     = models.CharField(max_length = 100, default = "")
-    password    = models.CharField(max_length = 100, default = "")
-    birthday    = models.CharField(max_length = 100, null = True)
-    gender_type = models.ForeignKey('Gender', on_delete = models.CASCADE, null=True)
-    email       = models.CharField(max_length = 200, default = "")
-    phoneNumber = models.CharField(max_length = 100, default = "") 
-    skinType    = models.ForeignKey('SkinType', on_delete = models.CASCADE, null=True)
-    skinTrouble = models.ManyToManyField('SkinTrouble', through = 'UserSkinTrouble')
+    name            = models.CharField(max_length = 50, default = "")
+    account         = models.CharField(max_length = 100, default = "")
+    password        = models.CharField(max_length = 100, default = "")
+    birthday        = models.CharField(max_length = 100, null = True)
+    gender_type     = models.ForeignKey('Gender', on_delete = models.CASCADE, null = True)
+    email           = models.CharField(max_length = 200, default = "")
+    phoneNumber     = models.CharField(max_length = 100, default = "")
+    skinType        = models.ForeignKey('SkinType', on_delete = models.CASCADE, null = True)
+    skinTrouble     = models.ManyToManyField('SkinTrouble', through = 'UserSkinTrouble')
+    likeProduct     = models.ManyToManyField(Product, through = 'LikeProduct', related_name = 'like_product')
     skinSensitivity = models.IntegerField(default = 0)
-    created_at  = models.DateTimeField(auto_now_add = True, null=True)
-    updated_at  = models.DateTimeField(auto_now = True, null=True)
+    created_at      = models.DateTimeField(auto_now_add = True, null = True)
+    updated_at      = models.DateTimeField(auto_now = True, null = True)
     
     class Meta:
         db_table = "users"
@@ -30,9 +32,9 @@ class SkinType(models.Model):
         db_table = "skin_types"
 
 class UserSkinTrouble(models.Model):
-    user = models.ForeignKey('User', on_delete = models.CASCADE)
+    user        = models.ForeignKey('User', on_delete = models.CASCADE)
     skinTrouble = models.ForeignKey('SkinTrouble', on_delete = models.CASCADE)
-
+    
     class Meta:
         db_table = "user_skin_troubles"
 
@@ -41,4 +43,18 @@ class SkinTrouble(models.Model):
 
     class Meta:
         db_table = "skin_troubles"
+
+class LikeProduct(models.Model):
+    user    = models.ForeignKey(User, on_delete = models.CASCADE)
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = "likeproducts"
+
+class CartProduct(models.Model):
+    user    = models.ForeignKey(User, on_delete = models.CASCADE)
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = "cartproducts"
 
--- user/views.py
+++ user/views.py
@@ -9,15 +9,18 @@ from django.http      import (
     JsonResponse
 )    
 
-from my_settings      import SECRET_KEY
-from user.models      import (
+from core.utils     import Login_required
+from my_settings    import SECRET_KEY
+from .models        import (
     User, 
     Gender, 
-    SkinType,
-    UserSkinTrouble,
-    SkinTrouble
-)
-from core.utils import login_decorator
+    SkinType, 
+    SkinTrouble, 
+    UserSkinTrouble, 
+    LikeProduct, 
+    CartProduct
+)    
+from product.models import *
 
 
 class SignUpView(View):
@@ -27,25 +30,22 @@ class SignUpView(View):
             if User.objects.filter(account = user_data['account']).exists():
                 return JsonResponse({'message':'DUPLICATED ID'}, status=400)
 
-
             User(
-                name        = user_data['name'],
-                account     = user_data['account'],
-                password    = bcrypt.hashpw(user_data['password'].encode('utf-8'), bcrypt.gensalt()).decode(),
-                email       = user_data['email'],
-                birthday    = user_data['birthday'],
-                gender_type = Gender.objects.get(name = user_data['gender_type']),
-                phoneNumber = user_data['phoneNumber'],
-                skinType = SkinType.objects.get(name = user_data['skinType']),
+                name            = user_data['name'],
+                account         = user_data['account'],
+                password        = bcrypt.hashpw(user_data['password'].encode('utf-8'), bcrypt.gensalt()).decode(),
+                email           = user_data['email'],
+                birthday        = user_data['birthday'],
+                gender_type     = Gender.objects.get(name = user_data['gender_type']),
+                phoneNumber     = user_data['phoneNumber'],
+                skinType        = SkinType.objects.get(name = user_data['skinType']),
                 skinSensitivity = user_data['skinSensitivity']
             ).save()
 
             UserSkinTrouble(
-                user = User.objects.get(account = user_data['account']),
+                user        = User.objects.get(account = user_data['account']),
                 skinTrouble = SkinTrouble.objects.get(name = user_data['skinTrouble'])
-
             ).save()
-
 
             return JsonResponse({'message':'SIGNUP SUCCESS'}, status=200)
         
@@ -61,7 +61,7 @@ class SignInView(View):
                 user = User.objects.get(account = user_data['account'])
  
                 if bcrypt.checkpw(user_data['password'].encode('utf-8'), user.password.encode('utf-8')):
-                    token = jwt.encode({'user_id':user.id}, 'secret', algorithm='HS256').decode('utf-8')
+                    access_token = jwt.encode({'account':user_data['account']}, 'secret', algorithm='HS256').decode('utf-8')
                     return JsonResponse({'access_token':access_token}, status=200)
                 
                 return JsonResponse({'message':'UNAUTHORIZED'}, status=401)
@@ -73,14 +73,86 @@ class SignInView(View):
             
 
 class LikeProductView(View):
-    @login_decorator
-    def get(self, request):
-        return JsonResponse({'like_list':list(Product.objects.values())}, status=200)
-
+    @Login_required
     def post(self, request):
+        user_data = json.loads(request.body)
+
+        if not LikeProduct.objects.filter(product_id = user_data['product_id'], user_id = request.user.id).exists():
+            LikeProduct.objects.create(
+                user    = request.user,
+                product = Product.objects.get(id = user_data['product_id'])
+            ).save
+
+            return JsonResponse({'message':'ADD LIKE_PRODUCT'}, status=200)
+
+        else:
+            delete_product = LikeProduct.objects.get(product_id = user_data['product_id'], user_id = request.user.id)
+            delete_product.delete()
+
+            return JsonResponse({'message':'DELETE LIKE_PRODUCT'}, status=200)
+
+    @Login_required 
+    def get(self, request):
+        like_list    = []
+        like_product = LikeProduct.objects.filter(user_id = request.user.id).prefetch_related("product")
         
 
+        for product in like_product:
+            data = {
+                "id"           : product.product.id,
+                "product_line" : product.product.product_line,
+                "name"         : product.product.name,
+                "price"        : product.product.price,
+                "sale_price"   : product.product.sale_price,
+                "hash_tag"     : product.product.hash_tag,
+                "images"       : product.product.image_set.first().image_url,
+                "sale"         : product.product.productflag_set.first().sale_flag,
+                "gift"         : product.product.productflag_set.first().gift_flag
+            }
 
-        
+            like_list.append(data)
+
+        return JsonResponse({'like_list' : like_list})
+
+class CartProductView(View):
+    @Login_required
+    def post(self, request):
+        user_data = json.loads(request.body)
+        try:
+            if not CartProduct.objects.filter(product_id = user_data['product_id'], user_id = request.user.id).exists():
+                CartProduct.objects.create(
+                    user    = request.user,
+                    product = Product.objects.get(id = user_data['product_id'])
+                ).save
+
+                return JsonResponse({'message':'ADD CART_PRODUCT'}, status=200) 
+            else:
+                return JsonResponse({'message': 'ALREADY_EXIST_PRODUCT'}, status=400)
+
+        except KeyError:
+            return JsonResponse({'message':'KEYERROR'}, status=200)
+
+    @Login_required
+    def get(self, request):
+        cart_list    = []
+        cart_product = CartProduct.objects.filter(user_id = request.user.id).prefetch_related("product")
+
+        for product in cart_product:
+            data = {
+                "id"           : product.product.id,
+                "product_line" : product.product.product_line,
+                "name"         : product.product.name,
+                "price"        : product.product.price,
+                "sale_price"   : product.product.sale_price,
+                "hash_tag"     : product.product.hash_tag,
+                "images"       : product.product.image_set.first().image_url,
+                "sale"         : product.product.productflag_set.first().sale_flag,
+                "gift"         : product.product.productflag_set.first().gift_flag
+
+            }
+
+            cart_list.append(data)
+
+        return JsonResponse({'cart_list' : cart_list})
 
 
