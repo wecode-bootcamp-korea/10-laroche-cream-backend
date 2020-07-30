85127bb09babbc53e06226eb3939410ad09a0d51
sol ahn <solahn@MacBook-Pro-5.local>
2020-07-30 21:46:22 +0900

    Add: 로그인 인증 데코레이터 추가

--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import (
+    HttpResponse, 
+    JsonResponse
+)
+from .models import ProductDetailTag
 
-# Create your views here.
+
+class ProductDetailTagView(View):
+    def get(self, request, product_id):
+        html_tag = ProductDetailTag.objects.get(id=product_id)
+        print("html_tag : ", html_tag)
+        
+        data = {
+            "id" : html_tag.id,
+            "banner" : html_tag.banner,
+            "detailView" : html_tag.productDetail,
+            "detailMid" : html_tag.sec3,
+            "detailPoint" : html_tag.sec5 ,
+            "research" : html_tag.sec6 ,
+            "PayInfo" : html_tag.purchaseView
+        }
+
+        return JsonResponse({'data': data})
+
+
